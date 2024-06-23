c19e58fb6e7bd821f6c04797fc552052fa9afb09
周诚 <zhoucheng133@outlook.com>
2024-06-23 22:40:25 +0800

    使用pywsgi

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, send_from_directory
+from gevent import pywsgi
 
 app = Flask(__name__, static_folder='dist', static_url_path='')
 
@@ -7,4 +8,6 @@ def serve_index():
     return send_from_directory(app.static_folder, 'index.html')
 
 if __name__ == '__main__':
-    app.run(debug=False, port=5000)
+    # app.run(debug=False, port=5000)
+    server=pywsgi.WSGIServer(('0.0.0.0', 5000), app)
+    server.serve_forever()
