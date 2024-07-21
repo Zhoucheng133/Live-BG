2f0ffc82ae5b522f2e16e11d068cb61bed8aa29c
周诚 <zhoucheng133@outlook.com>
2024-07-21 17:59:29 +0800

    添加python的端口api

--- app.py
+++ app.py
@@ -3,12 +3,19 @@ from gevent import pywsgi
 
 app = Flask(__name__, static_folder='dist', static_url_path='')
 
+port=9098
+
 @app.route('/')
 def serve_index():
     return send_from_directory(app.static_folder, 'index.html')
 
+@app.route('/api/port')
+def serve_port():
+    global port
+    return port
+
 if __name__ == '__main__':
-    # app.run(debug=False, port=8080)
+    port=input("输入端口号 (默认为9098): ")
     print("服务运行在: http://127.0.0.1:5000")
     server=pywsgi.WSGIServer(('0.0.0.0', 5000), app, log=None)
     server.serve_forever()
